4d3b2630c0bb3f707cfe2b3540689dea840ec159
albhasan <albhasan@gmail.com>
2023-08-17 14:29:56 -0300

    NeoVim configuration files re-ordered. Added config files for NvChad.

---
+++ nvim/NvChad/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,35 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.pyright.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+})
+
+lspconfig.clangd.setup {
+    on_attach = function(client, bufnr)
+        client.server_capabilities.signatureHelpProvider = false
+        on_attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+}
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"rust"},
+    root_dir = util.root_pattern("Cargo.toml"),
+    settings = {
+        ['rust_analyzer'] = {
+            cargo = {
+                allFeatures = true,
+      }
+    }
+  }
+})
