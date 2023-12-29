8bc8217cc4bed3e28885b601e5ef3e6b0f4537bb
albhasan <albhasan@gmail.com>
2023-12-28 22:12:53 -0300

    Neovim configuration using typecraft tutorial

---
+++ nvim/typecraft/lua/plugins/none-ls.lua
@@ -0,0 +1,21 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            source = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.diagnostics.rubocop,
+                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.diagnostics.shellcheck,
+            }
+        })
+
+        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+    end
+}
