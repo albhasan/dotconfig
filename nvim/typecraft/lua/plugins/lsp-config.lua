32b6d2cb662701f66bf67803369124ba53dc63ff
albhasan <albhasan@gmail.com>
2023-12-30 19:51:56 -0300

    Autocompletion & snippets added.

--- nvim/typecraft/lua/plugins/lsp-config.lua
+++ nvim/typecraft/lua/plugins/lsp-config.lua
@@ -1,28 +1,39 @@
 return {
-    {
-      "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
     },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
