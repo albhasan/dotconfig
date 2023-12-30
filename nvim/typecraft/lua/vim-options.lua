593258e1f70025920f528eba1af0cbca9f22c3ee
albhasan <albhasan@gmail.com>
2023-12-30 14:40:06 -0300

    Set defaults for colorcolumn clipboard.

--- nvim/typecraft/lua/vim-options.lua
+++ nvim/typecraft/lua/vim-options.lua
@@ -2,4 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = " "
+
