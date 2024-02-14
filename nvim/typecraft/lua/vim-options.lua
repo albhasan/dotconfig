f555304f1a67be9cc0b1e4befde6007e3981a569
albhasan <albhasan@gmail.com>
2024-02-14 14:41:48 -0300

    Updated to follow help recommendations.

--- nvim/typecraft/lua/vim-options.lua
+++ nvim/typecraft/lua/vim-options.lua
@@ -3,9 +3,10 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set colorcolumn=80")
-vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set clipboard+=unnamedplus")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
 
 vim.cmd("hi Normal guibg=NONE")
