bd435a1e48556bbddbf459e0ae682e1b9535538c
albhasan <albhasan@gmail.com>
2024-03-16 09:57:58 -0300

    eslint and shellcheck commented to avoid nvim warnings.

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,15 +4,15 @@ return {
         local null_ls = require("null-ls")
 
         null_ls.setup({
-            source = {
+            sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.rubocop,
                 null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.shellcheck,
+                --null_ls.builtins.diagnostics.eslint_d,
+                --null_ls.builtins.diagnostics.shellcheck
             }
         })
 
