0c246ad36282c722be21eef5b92e94d50e48f748
albhasan <albhasan@gmail.com>
2026-01-10 17:59:59 -0300

    Addec script to monitor PM 10/25 at SJC.

---
+++ bin/airquality.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/alber/Documents/software/softwareInstall/air_quality/venv/bin/activate
+cat /home/alber/Documents/software/softwareInstall/air_quality/README
+python /home/alber/Documents/software/softwareInstall/air_quality/script.py
+exit 0
